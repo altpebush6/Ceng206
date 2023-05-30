17fd861ebddffd602e917357d176c4f5c7b3099a
fthherdmm <86978984+fthherdmm@users.noreply.github.com>
2023-05-30 23:04:14 +0300

    Update movementJS.cpp

--- movementJS.cpp
+++ movementJS.cpp
@@ -16,4 +16,28 @@ class AMyCharacter : private ACharacter {
         PrimaryActorTick.bCanEverTick = true;
         bIsRunning = false;
     }
-   
+    AMyCharacter::~AMyCharacter()
+    {
+        try
+        {
+            cout << "Object is destroying" << WalkSpeed << RunSpeed << JumpHeight << endl:
+        }
+        catch (const std::exception& e)
+        {
+            UE_LOG(LogTemp, Error, TEXT("Hata: %s"), *FString(e.what()));
+        }
+    }
+   void AMyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+    {
+        Super::SetupPlayerInputComponent(PlayerInputComponent);
+     
+        PlayerInputComponent->BindAxis("MoveForward", this, &AMyCharacter::MoveForward);
+        PlayerInputComponent->BindAxis("MoveRight", this, &AMyCharacter::MoveRight);
+
+        // Koşma girişleri tanımlanır
+        PlayerInputComponent->BindAction("Run", IE_Pressed, this, &AMyCharacter::StartRunning);
+        PlayerInputComponent->BindAction("Run", IE_Released, this, &AMyCharacter::StopRunning);
+
+        // Zıplama girişi tanımlanır
+        PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &AMyCharacter::Jump);
+    }
