dc28fe9e9980989dd57d9fefcbade301ca00624c
biehcey <116308507+biehcey@users.noreply.github.com>
2023-05-30 22:53:14 +0300

    Update movementKeyboard.cpp

--- movementKeyboard.cpp
+++ movementKeyboard.cpp
@@ -1,3 +1,24 @@
 class AMyCharacter : private ACharacter {
-  
+   AMyCharacter::AMyCharacter()
+    {
+        
+        WalkSpeed = 300.0f;
+
+        
+        RunSpeed = 600.0f;
+
+        
+        JumpHeight = 500.0f;
+
+        try
+        {
+            
+            GetCharacterMovement()->MaxWalkSpeed = WalkSpeed;
+        }
+        catch (const std::exception& e)
+        {
+            
+            UE_LOG(LogTemp, Error, TEXT("Error: %s"), *FString(e.what()));
+        }
+    }
 };
