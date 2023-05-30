153e8b6a5a2406627c3ee95d3cc3f10da11ff6bd
ebu <93250409+altpebush6@users.noreply.github.com>
2023-05-30 23:08:29 +0300

    movement.h

---
+++ movement.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MyCharacter.generated.h"
+
+UCLASS()
+class MyProject2 AMyCharacter : public ACharacter
+{
+    GENERATED_BODY()
+
+public:
+    AMyCharacter();
+
+protected:
+    virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+    virtual void Tick(float DeltaSeconds) override;
+
+    // Speeds
+    UPROPERTY(EditDefaultsOnly, Category = "Movement")
+        float WalkSpeed;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Movement")
+        float RunSpeed;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Movement")
+        float JumpHeight;
+
+    // Action and Axis Events
+    void MoveForward(float Value);
+    void MoveRight(float Value);
+    void StartRunning();
+    void StopRunning();
+    void Jump();
+
+private:
+    bool bIsRunning;
+    int32 GhostCount;
+};
