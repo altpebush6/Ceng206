0d43809df995987dc639aa74515ed31a0142aec2
Emre Özdemir <64666072+necatiiemre@users.noreply.github.com>
2023-05-30 23:08:24 +0300

    Update ghostMovement.cpp
    ghost Movement randomized vector

--- ghostMovement.cpp
+++ ghostMovement.cpp
@@ -23,3 +23,17 @@ public:
             GetWorld()->SpawnActor<AMyGhostActor>(GetClass(), Location, Rotation);
         }
     }
+    virtual void Tick(float DeltaTime) override
+    {
+        Super::Tick(DeltaTime);
+
+        // Here, we determine the movement of the ghosts.
+        // For example, we can move the ghosts in different directions.
+        // You can update the positions of the ghosts randomly.
+        // We will update only the X and Y coordinates of the ghosts randomly.
+        FVector Location = GetActorLocation();
+        Location.X += FMath::RandRange(-100.f, 100.f) * DeltaTime;
+        Location.Y += FMath::RandRange(-100.f, 100.f) * DeltaTime;
+        SetActorLocation(Location);
+    }
+};
