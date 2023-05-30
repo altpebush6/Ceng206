c798855ef0daf8af97fe7b6fb094df442da2a1e5
Emre Özdemir <64666072+necatiiemre@users.noreply.github.com>
2023-05-30 23:45:05 +0300

    Update ghostMovement.cpp
    ghost size improved

--- ghostMovement.cpp
+++ ghostMovement.cpp
@@ -2,7 +2,7 @@
 #include "Actor.h"
 #include "movement.h"
 #include "vector.h"
-
+int32 ghostSize = 13;
 UCLASS()
 class YOURPROJECT_API AMyGhostActor : public AActor
 {
@@ -18,7 +18,7 @@ public:
     {
         Super::BeginPlay();
         // In the beginning, we place the locations of the ghosts randomly.
-        for (int32 i = 0; i < 13; ++i)
+        for (int32 i = 0; i < ghostSize; ++i)
         {
             FVector Location(FMath::RandRange(-1000.f, 1000.f), FMath::RandRange(-1000.f, 1000.f), 0.f);
             FRotator Rotation(0.f, FMath::RandRange(0.f, 360.f), 0.f);
